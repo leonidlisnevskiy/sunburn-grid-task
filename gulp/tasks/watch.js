5205698721b4cd3aea3d594679f0a4b4ed209b5c
leonidlisnevskiy <leonidlisnevskiy@gmail.com>
2018-11-09 14:46:17 +0200

    first commit

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,11 @@
+var gulp   = require('gulp');
+var config = require('../config');
+
+gulp.task('watch', 
+    ['copy:watch',
+    'jade:watch',
+    'sprite:png:watch',
+    'list-pages:watch',
+    'webpack:watch',
+    'sass:watch'
+]);
